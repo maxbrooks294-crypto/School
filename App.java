eec9c7c08e6ea77751242a6b6a207f6321ae3817
maxbrooks294-crypto <maxbrooks294@gmail.com>
2025-09-05 14:56:55 -0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! My name is Max Brooks, I am 15 years old. I have 7 siblings. In order from Oldest to youngest, My oldest sister nd oldest sibling, Jordan, age 35, My oldest brother Colt, age 33, My sister Chloe, age 30, the youngest girl, Bonnie, age 27, My Brother, Zane, age 24, My Brother, Zeke, age 21, and then Me. I am the youngest.");
     }
 }
